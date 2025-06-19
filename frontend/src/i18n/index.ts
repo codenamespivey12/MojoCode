c6c0a1b3326b8e0768759642b2ab4cb72747999f
codenamespivey12 <codenamespivey@sixtyoneeightyai.com>
2025-06-19 05:51:46 -0500

    feat(i18n): restrict translations to English

--- frontend/src/i18n/index.ts
+++ frontend/src/i18n/index.ts
@@ -3,22 +3,7 @@ import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 
-export const AvailableLanguages = [
-  { label: "English", value: "en" },
-  { label: "日本語", value: "ja" },
-  { label: "简体中文", value: "zh-CN" },
-  { label: "繁體中文", value: "zh-TW" },
-  { label: "한국어", value: "ko-KR" },
-  { label: "Norsk", value: "no" },
-  { label: "Arabic", value: "ar" },
-  { label: "Deutsch", value: "de" },
-  { label: "Français", value: "fr" },
-  { label: "Italiano", value: "it" },
-  { label: "Português", value: "pt" },
-  { label: "Español", value: "es" },
-  { label: "Türkçe", value: "tr" },
-  { label: "Українська", value: "uk" },
-];
+export const AvailableLanguages = [{ label: "English", value: "en" }];
 
 i18n
   .use(Backend)
